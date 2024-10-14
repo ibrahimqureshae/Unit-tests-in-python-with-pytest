54a43248fdf7716f0518cdab5e352af05ad5e396
Muhammad Ibrahim <ibrahim.qureshae@gmail.com>
2024-10-14 21:18:54 +0200

    Mock functions - unittest

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,4 +1,6 @@
+from unittest.mock import Mock
 import pytest
+from item_database import ItemDatabase
 from shoppingcart import ShoppingCart
 
 #setup the duplicate code in your tests
@@ -24,12 +26,22 @@ def test_when_item_added_then_cart_contains_item(cart):
 def test_when_add_more_items_than_max_items_should_fail(cart):
     for i in range(5):
         cart.add("apple")
+
     with pytest.raises(OverflowError):
         cart.add("apple")
         
 def test_can_get_total_price(cart):
     cart.add("apple")
     cart.add("orange")
-    price_map = {"apple": 1.0, "orange": 2.0 }
-    assert cart.get_total_price(price_map) == 3.0
+    item_database = ItemDatabase()
+    
+    def mock_get_item(item: str):
+        if item == "apple":
+            return 1.0
+        if item == "orange":
+            return 2.0
+        
+    item_database.get = Mock(side_effect = mock_get_item)
+    assert cart.get_total_price(item_database) == 3.0
+
     
